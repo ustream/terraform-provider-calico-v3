6721ce6b18a16fbeb8ca3c76165fe3c25a4b63dd
n-marton <marton@natko.hu>
2018-08-21 09:20:14 +0200

    renaming provider to handle conflict with v1 api's provider

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"github.com/cdiscount/terraform-provider-calico/calico"
 	"github.com/hashicorp/terraform/plugin"
 	"github.com/hashicorp/terraform/terraform"
+	calicov3 "github.com/ustream/terraform-provider-calico-v3/calico"
 )
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: func() terraform.ResourceProvider {
-			return calico.Provider()
+			return calicov3.Provider()
 		},
 	})
 }
